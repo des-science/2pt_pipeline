d44679d1afdca9a8a3eece80ee09e5f07e79d6fd
troxel <michael.a.troxel@gmail.com>
2018-04-02 11:47:01 -0400

    fix

--- pipeline/nofz.py
+++ pipeline/nofz.py
@@ -647,6 +647,10 @@ class nofz(PipelineStage):
 
         self.mean_e1 = []
         self.mean_e2 = []
+        self.sigma_e = []
+        self.sigma_ec = []
+        self.neff = []
+        self.neffc = []
         e1_  = self.selector_mcal.get_col(self.Dict.shape_dict['e1'],nosheared=True)[0]
         e2_  = self.selector_mcal.get_col(self.Dict.shape_dict['e2'],nosheared=True)[0]
         cov00_  = self.selector_mcal.get_col(self.Dict.shape_dict['cov00'],nosheared=True)[0]
