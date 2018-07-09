ff05504bb837330a774cb2ac061464c56fa0e1f1
troxel <michael.a.troxel@gmail.com>
2018-07-09 15:57:11 -0400

    fixed lens selection

--- pipeline/measure_2pt.py
+++ pipeline/measure_2pt.py
@@ -331,8 +331,8 @@ class Measure2Point(PipelineStage):
             # Get tomographic bin masks for lenses and randoms, and weights
             R1,R2,mask,w,rmask = self.get_zbins_R(i,cal,shape=False)
             # Get index slices needed for the subset of healpixels in this calculation
-            print len(pix),len(gmask),len(cal.selector.mask),len(mask),np.sum(mask)
-            pixrange,pixrange2 = get_pix_subset(ipix,pix[gmask][cal.selector.mask][mask],return_neighbor)
+            print len(pix),len(gmask),len(cal.selector.mask[self.Dict.ind['u']]),len(mask),np.sum(mask)
+            pixrange,pixrange2 = get_pix_subset(ipix,pix[gmask][cal.selector.mask[self.Dict.ind['u']]][mask],return_neighbor)
 
             # Load ra,dec from gold catalog - source.read is necessary for the raw array to downmatch to lens catalog
             ra  = self.gold_selector.source.read(self.Dict.gold_dict['ra'])[self.Dict.ind['u']]
