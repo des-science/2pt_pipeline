8f57e3d630606f3bbfcdb816ea353166a793c4bb
troxel <michael.a.troxel@gmail.com>
2018-08-31 10:01:40 -0400

    push

--- pipeline/nofz.py
+++ pipeline/nofz.py
@@ -297,7 +297,7 @@ class nofz(PipelineStage):
             # Stack scalar values into n(z) looping over tomographic bins
             for i in range(zbins):
                 # Get array masks for the tomographic bin for unsheared and sheared catalogs
-                print i,xbins,edge,len(xbins),np.sum(xbins == i),np.sum((bin_col>edge[i])&(bin_col<edge[i+1]))
+                print i,xbins,edge,bin_col,len(xbins),np.sum(xbins == i),np.sum((bin_col>edge[i])&(bin_col<edge[i+1]))
                 mask        =  (xbins == i)
                 if shape:
                     if self.params['has_sheared']:
