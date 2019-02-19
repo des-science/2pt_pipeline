aa8e8e7c35858fc01e687ecc3effdc8a67b05706
Alexandra Amon <aamon@cori10.nersc.gov>
2019-02-19 15:48:54 -0800

    I thinkt that pipeline/ shouldn't be there as already in that directory

--- pipeline/BLIND.sh
+++ pipeline/BLIND.sh
@@ -1,1 +1,1 @@
-python pipeline/blind_2pt_usingcosmosis.py  -i pipeline/blinding_params_template.ini -b add -u pipeline/2pt_sim_1110_baseline_Y3cov.fits
+python blind_2pt_usingcosmosis.py  -i blinding_params_template.ini -b add -u 2pt_sim_1110_baseline_Y3cov.fits
