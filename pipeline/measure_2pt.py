985becf75df43e35cf2c4d0c5e8e4b13550881f9
matroxel <michael.a.troxel@gmail.com>
2019-06-07 10:16:21 -0400

    Update measure_2pt.py

--- pipeline/measure_2pt.py
+++ pipeline/measure_2pt.py
@@ -23,13 +23,14 @@ destest_dict_ = {
     'dg'            : 0.01
     }
 
-def create_destest_yaml( params, cal_type, group, table, select_path, name_dict ):
+def create_destest_yaml( params, name, cal_type, group, table, select_path, name_dict ):
     """
     Creates the input dictionary structure from a passed dictionary rather than reading froma yaml file.
     """
 
     destest_dict = destest_dict_.copy()
     destest_dict['load_cache'] = params['load_cache']
+    destest_dict['name'] = name
     destest_dict['output'] = params['output']
     destest_dict['filename'] = params['datafile']
     destest_dict['param_file'] = params['param_file']
@@ -42,12 +43,12 @@ def create_destest_yaml( params, cal_type, group, table, select_path, name_dict 
 
     return destest_dict
 
-def load_catalog(pipe_params, cal_type, group, table, select_path, name_dict, inherit=None, return_calibrator=None):
+def load_catalog(pipe_params, name, cal_type, group, table, select_path, name_dict, inherit=None, return_calibrator=None):
     """
     Loads data access and calibration classes from destest for a given yaml setup file.
     """
     # Input yaml file defining catalog
-    params = create_destest_yaml(pipe_params, cal_type, group, table, select_path, name_dict)
+    params = create_destest_yaml(pipe_params, name, cal_type, group, table, select_path, name_dict)
     # Load destest source class to manage access to file
     source = destest.H5Source(params)
     # Load destest selector class to manage access to data in a structured way
@@ -104,11 +105,11 @@ class Measure2Point(PipelineStage):
         print 'using dictionary: ',self.params['dict_file']
                 
         # Load data and calibration classes
-        self.source_selector, self.source_calibrator = load_catalog(self.params, 'mcal', self.params['source_group'], self.params['source_table'], self.params['source_path'], self.Dict, return_calibrator=destest.MetaCalib)
-        self.lens_selector, self.lens_calibrator     = load_catalog(self.params, None, self.params['lens_group'], self.params['lens_table'], self.params['lens_path'], self.Dict, return_calibrator=destest.NoCalib)
-        self.gold_selector = load_catalog(self.params, 'mcal', self.params['gold_group'], self.params['gold_table'], self.params['gold_path'], self.Dict, inherit=self.source_selector)
-        self.pz_selector   = load_catalog(self.params, 'mcal', self.params['pz_group'], self.params['pz_table'], self.params['pz_path'], self.Dict,   inherit=self.source_selector)
-        self.ran_selector  = load_catalog(self.params, None, self.params['ran_group'], self.params['ran_table'], self.params['ran_path'], self.Dict)
+        self.source_selector, self.source_calibrator = load_catalog(self.params, 'mcal', 'mcal', self.params['source_group'], self.params['source_table'], self.params['source_path'], self.Dict, return_calibrator=destest.MetaCalib)
+        self.lens_selector, self.lens_calibrator     = load_catalog(self.params, 'lens', None, self.params['lens_group'], self.params['lens_table'], self.params['lens_path'], self.Dict, return_calibrator=destest.NoCalib)
+        self.gold_selector = load_catalog(self.params, 'gold', 'mcal', self.params['gold_group'], self.params['gold_table'], self.params['gold_path'], self.Dict, inherit=self.source_selector)
+        self.pz_selector   = load_catalog(self.params, 'pz', 'mcal', self.params['pz_group'], self.params['pz_table'], self.params['pz_path'], self.Dict,   inherit=self.source_selector)
+        self.ran_selector  = load_catalog(self.params, 'ran', None, self.params['ran_group'], self.params['ran_table'], self.params['ran_path'], self.Dict)
 
         self.Dict.ind = self.Dict.index_dict #a dictionary that takes unsheared,sheared_1p/1m/2p/2m as u-1-2-3-4 to deal with tuples of values returned by get_col()
         
