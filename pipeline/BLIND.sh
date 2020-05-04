315c477c70e89635598fc3038f8a7c42dfca46c5
Alexandra Amon <Alex>
2020-05-04 10:41:18 -0700

    updating BLIND for redmagic specific params

--- pipeline/BLIND.sh
+++ pipeline/BLIND.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 export DEMODEL=lcdm
 echo $1
-python pipeline/blind_2pt_usingcosmosis.py  -i pipeline/blinding_params_template.ini -b add -u ${1}
+python pipeline/blind_2pt_usingcosmosis.py  -i pipeline/blinding_params_redmagic.ini -b add -u ${1}
 rm $1
